e9c3f68060f43f0eeeb881bb9fb665a05dc9e797
Elin Angelov <opensuser@gmail.com>
2021-11-19 15:24:44 +0200

    fix: base path

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ module.exports = (swaggerDocument, pathName, methodName) => {
                 if (!pathName.startsWith('x-')) {
                     Object.entries(path).forEach(([methodName, method]) => {
                         if (methodName !== 'parameters' && !methodName.startsWith('x-')) {
-                            validators[method.operationId] = methodValidator(swagger, pathName, methodName);
+                            validators[method.operationId] = methodValidator(swagger, basePath + pathName, methodName);
                         }
                     });
                 }
